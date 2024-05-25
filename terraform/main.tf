462b8dcbc1b8870ea7e6c4eaeedd0b7d0c54464c
jahnvigangwar <jahnvigangwar@gmail.com>
2024-05-25 10:02:46 +0530

    remove secrets from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  region = "ap-northeast-1"
-  access_key = "AKIA6GBMBHOYU2RWXCM3"
-  secret_key = "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"
-}
-
-
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
   acl    = var.acl
